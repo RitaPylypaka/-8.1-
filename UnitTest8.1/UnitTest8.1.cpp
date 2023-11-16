4dee79bf4c014aa7a26a4431a8048d45163d158e
RitaPylypaka <ritapylypaka@gmail.com>
2023-11-16 12:32:02 +0200

    Add project files.

---
+++ UnitTest8.1/UnitTest8.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "CppUnitTest.h"
+#include "../пр8.1іт/8.1іт.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81
+{
+	TEST_CLASS(UnitTest81)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		char str[] = { ".s..g." };
+		int test = Count(str);
+		Assert::AreEqual(0, test);
+					}
+				};
+			}
+			
---
+++ UnitTest8.1/pch.cpp
@@ -0,0 +1,9 @@
+// pch.cpp: source file corresponding to the pre-compiled header
+
+#include "pch.h"
+#ifndef PCH_H
+#define PCH_H
+
+#endif
+
+// When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
